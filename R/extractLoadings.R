a860c3704dfd388aebef46e69f3d8e1d2a35e329
mikemartinez99 <mike.j.martinez99@gmail.com>
2025-10-16 09:45:08 -0400

    Add extractEigen function

--- R/extractLoadings.R
+++ R/extractLoadings.R
@@ -1,36 +1,33 @@
-#' extractLoadings
+#' extractEigen
 #'
-#' @description Extract gene loadings for PCs of interest and calculate percent variance at the per-gene level
+#' @description Extract gene-level Eigen-vectors for PCs of interest and calculate percent variance.
 #'
-#' @import ComplexHeatmap
-#' @import scales
-#' @import RColorBrewer
-#' @import magick
-#' @import grid
-#'
-#' @param variance A vector of gene variances (can calculate using RGenEDA::plotVariance)
-#' @param pcRes Output list from RGenEDA::generatePCs
+#' @param object A `geneda` object containing PCA results in the `DimReduction` slot
 #' @param component Principal component to use (i.e., PC1)
-#' @param nfeatures Number of features principal components were generated on
 #'
 #' @returns A data-frame containing gene loading information and percent variation per gene
 #' @export
+extractEigen <- function(object, component) {
+  stopifnot(methods::is(object, "geneda"))
 
+  if (length(object@DimReduction) == 0L) {
+    message("DimReduction slot is empty. Please use RunPCA.")
+  }
+  eigenvecs <- object@DimReduction[["Eigenvectors"]][[component]]
+  percentVar <- object@DimReduction[["percent_var"]][[component]]
+  geneNames <- rownames(object@DimReduction$Eigenvectors)
 
-extractLoadings <- function(variance, pcRes, component, nfeatures) {
-  loadings <- pcRes[["Eigenvectors"]][,component]
-  gene_names <- names(variance)[1:nfeatures]
+  # Get gene-level variance
+  pcVar <- as.numeric(gsub("%", "", percentVar))/100
+  genePercent <- (eigenvecs^2 * pcVar) / sum(eigenvecs^2 * pcVar) * 100
 
-  #== Get gene level variance
-  pc_var <- pcRes[["percent_var"]][component]
-  pc_var_numeric <- as.numeric(gsub("%","",pc_var))/100
-  gene_percent <- (loadings^2 * pc_var_numeric) / sum(loadings^2 * pc_var_numeric) * 100
-
+  # Save as dataframe
   df <- data.frame(
-    Gene = gene_names,
-    Loading = loadings,
-    PercentVariance = gene_percent
+    Gene = geneNames,
+    EigenVector = eigenvecs,
+    PctVar = genePercent
   )
 
   return(df)
+
 }
